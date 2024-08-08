94961be9a825e20f609cf2e0a96c012a24de49e5
VishalBargal <109430773+VishalBargal@users.noreply.github.com>
2024-08-08 17:02:53 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql');
 
 
-const con = mysql.createConnection({
-  host: 'sql12.freemysqlhosting.net',
-  user: 'sql12724400',
-  password: 'IUhQkRsIZd',
-  database: 'sql12724400',
-  port:'3306'
-});
 
-// const con = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'registerLogin',
-// });
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'registerLogin',
+});
 
 con.connect((err) => {
   if (err) {
